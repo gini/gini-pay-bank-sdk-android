bae7556842791d2ba36e91de1fd8d860626416cd
Alexandru Gabor <alexandru.gabor@halcyonmobile.com>
2021-05-04 09:05:20 +0300

    Improve pay feature in example app

---
+++ ginipaybank/src/main/java/net/gini/pay/bank/pay/Business.kt
@@ -0,0 +1,23 @@
+package net.gini.pay.bank.pay
+
+import android.content.Intent
+import android.net.Uri
+import net.gini.android.models.PaymentRequest
+
+internal const val Scheme = "ginipay" // It has to match the scheme in query tag in manifest
+private const val PaymentPath = "payment"
+
+fun getRequestId(intent: Intent): String {
+    check(intent.action == Intent.ACTION_VIEW) { "Intent has wrong action" }
+    val uri = intent.data
+    check(uri != null) { "Intent has wrong action" }
+    check(uri.scheme == Scheme) { "Intent has wrong scheme" }
+    val path = uri.pathSegments
+    check(path != null && path.size == 1 && uri.authority == PaymentPath) { "Intent has wrong path" }
+    return path[0]
+}
+
+fun PaymentRequest.getBusinessIntent() = Intent().apply {
+    action = Intent.ACTION_VIEW
+    data = Uri.parse(this@getBusinessIntent.requesterUri)
+}
---
+++ ginipaybank/src/main/java/net/gini/pay/bank/pay/GiniBank.kt
@@ -0,0 +1,24 @@
+package net.gini.pay.bank.pay
+
+import android.content.Context
+import net.gini.android.Gini
+import net.gini.android.models.PaymentRequest
+import net.gini.android.models.ResolvePaymentInput
+
+class GiniBank(
+    val giniApi: Gini,
+) {
+    private val documentManager = giniApi.documentManager
+
+    suspend fun getPaymentRequest(id: String): PaymentRequest {
+        return documentManager.getPaymentRequest(id)
+    }
+
+    suspend fun resolvePaymentRequest(requestId: String, resolvePaymentInput: ResolvePaymentInput): String {
+        return documentManager.resolvePaymentRequest(requestId, resolvePaymentInput)
+    }
+
+    fun returnToBusiness(context: Context, paymentRequest: PaymentRequest) {
+        context.startActivity(paymentRequest.getBusinessIntent())
+    }
+}
