b13673aa0f40fd5a8e08c1d2d53617cd79a54de5
Geonosis Droid Factory <mobilecd@GeonosiMobileCD.office.gini.net>
2021-07-09 15:09:08 +0200

    Gini Pay Bank SDK KDoc

--- scripts/sourceset_dependencies.js
+++ scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":ginipaybank:dokkaHtml/androidTestRelease": [], ":ginipaybank:dokkaHtml/debug": [], ":ginipaybank:dokkaHtml/main": [], ":ginipaybank:dokkaHtml/release": []}'
+sourceset_dependencies='{":ginipaybank:dokkaHtml/androidTestRelease":[],":ginipaybank:dokkaHtml/debug":[],":ginipaybank:dokkaHtml/main":[],":ginipaybank:dokkaHtml/release":[]}'
